3a318597b3ec0b11a7ea384f2a49ef3e41205d69
Jon Kinney <jonkinney@gmail.com>
2024-08-21 10:01:01 -0500

    feat: update lazy_setup.lua enable local dev mode
    Also provide some helper code to check if we need to run Lazy clean to
    clear the cache.

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -1,6 +1,8 @@
 require("lazy").setup({
   {
     "AstroNvim/AstroNvim",
+    -- dev = true,
+    -- dir = vim.fn.expand "~/Code/AstroNvim",
     version = "^4", -- Remove version tracking to elect for nighly AstroNvim
     import = "astronvim.plugins",
     opts = { -- AstroNvim options must be set here with the `import` key
@@ -33,3 +35,33 @@ require("lazy").setup({
     },
   },
 } --[[@as LazyConfig]])
+
+local astronvim_config = require("lazy.core.config").plugins["AstroNvim"]
+if astronvim_config and astronvim_config.dev == true and astronvim_config.dir then
+  local lazy_config = require "lazy.core.config"
+
+  local default_install_path = lazy_config.options.root .. "/AstroNvim"
+  if vim.fn.isdirectory(default_install_path) == 1 then
+    vim.schedule(function()
+      local choice = vim.fn.confirm("AstroNvim needs cleaning. Run :Lazy clean?", "&Yes\n&No", 2)
+      if choice == 1 then
+        require("lazy").clean()
+        vim.notify("AstroNvim has been cleaned", vim.log.levels.INFO, {
+          title = "AstroNvim Development Build",
+          icon = "✨",
+        })
+
+        -- Prompt for restart
+        vim.notify("Please restart Neovim manually to use the development version.", vim.log.levels.WARN, {
+          title = "AstroNvim Development Build",
+          icon = "⚠️",
+        })
+      end
+    end)
+  else
+    vim.notify("RUNNING LOCAL ASTRONVIM FROM " .. astronvim_config.dir, vim.log.levels.WARN, {
+      title = "AstroNvim Development Build",
+      icon = "🚧",
+    })
+  end
+end
