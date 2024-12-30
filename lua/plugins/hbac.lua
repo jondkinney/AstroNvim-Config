9190ff371203c80a63b9a234a79e830620acaf80
Jon Kinney <jonkinney@gmail.com>
2024-12-30 11:19:06 -0600

    plug: hbac auto close unused buffers over time

---
+++ lua/plugins/hbac.lua
@@ -0,0 +1,11 @@
+-- ╭──────────────────────────────────────────────────────────────────────────╮
+-- │              Automatically closes unused buffers over time               │
+-- ╰──────────────────────────────────────────────────────────────────────────╯
+
+-- https://github.com/axkirillov/hbac.nvim
+-- ━━ Plugin URL ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+return {
+  "axkirillov/hbac.nvim",
+  config = true,
+}
