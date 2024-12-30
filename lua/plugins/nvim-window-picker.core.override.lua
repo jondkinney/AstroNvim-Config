008cf384c75f2fa6e5dba7a442201613088635b5
Jon Kinney <jonkinney@gmail.com>
2024-12-30 11:43:44 -0600

    core: nvim-window-picker override bar colors

---
+++ lua/plugins/nvim-window-picker.core.override.lua
@@ -0,0 +1,33 @@
+return {
+  "s1n7ax/nvim-window-picker",
+  opts = {
+    -- You can pass in the highlight name or a table of content to set as
+    -- highlight
+    highlights = {
+      statusline = {
+        focused = {
+          fg = "#ECEFF4",
+          bg = "#BF616A",
+          bold = true,
+        },
+        unfocused = {
+          fg = "#2E3440",
+          bg = "#A3BE8C",
+          bold = true,
+        },
+      },
+      winbar = {
+        focused = {
+          fg = "#ECEFF4",
+          bg = "#BF616A",
+          bold = true,
+        },
+        unfocused = {
+          fg = "#2E3440",
+          bg = "#A3BE8C",
+          bold = true,
+        },
+      },
+    },
+  },
+}
